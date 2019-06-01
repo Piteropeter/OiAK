105b24f02b9c6c13d759e53f6b8540865ee316fc
Piotr <piteropeter@gmail.com>
2019-06-01 12:35:06 +0200

    unit_tests linux compatibility

---
+++ prepare_sdk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install make
+sudo apt-get install cmake
+sudo apt-get install libgtest-dev
+cd /usr/src/googletest/googletest
+sudo mkdir build
+cd build
+sudo cmake ..
+sudo make
+sudo cp libgtest* /usr/lib/
+cd ..
+sudo rm -rf build
