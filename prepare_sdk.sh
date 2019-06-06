8da8e582803035f99665dcb5de3254a0d6a7bf51
Piotr <piteropeter@gmail.com>
2019-06-06 12:01:16 +0200

    Prepare_sdk update

--- prepare_sdk.sh
+++ prepare_sdk.sh
@@ -2,14 +2,14 @@
 sudo apt-get update
 sudo apt-get install gcc
 sudo apt-get install g++
+sudo apt-get install git
 sudo apt-get install make
 sudo apt-get install cmake
-sudo apt-get install libgtest-dev
-cd /usr/src/googletest/googletest
-sudo mkdir build
+git clone https://github.com/microsoft/vcpkg.git
+vcpkg/bootstrap-vcpkg.sh
+vcpkg/vcpkg install gtest
+vcpkg/vcpkg install benchmark
+vcpkg/vcpkg integrate install
+mkdir build
 cd build
-sudo cmake ..
-sudo make
-sudo cp libgtest* /usr/lib/
-cd ..
-sudo rm -rf build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DENABLE_UNIT_TESTS=ON -DENABLE_BENCHMARKS=ON
